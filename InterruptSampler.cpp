10889d570447de9bf4336a68dca9229c1bd0f823
EAD Fritz <ead.fritz@gmail.com>
2015-01-21 10:14:45 +0100

    added due compability

--- InterruptSampler.cpp
+++ InterruptSampler.cpp
@@ -1,6 +1,14 @@
 #include "InterruptSampler.h"
 #include "Arduino.h"
 
+#if defined (__arm__) && defined (__SAM3X8E__) // Arduino Due compatible
+const uint16_t IS_BUFFER_SIZE=1024; //8192 bits
+const uint16_t IS_BUFFER_MASK=0x1FFF;
+#else
+const uint16_t IS_BUFFER_SIZE =512;  //4096 bits
+const uint16_t IS_BUFFER_MASK=0x0FFF;
+#endif
+
 volatile uint32_t is_now = 0;
 volatile uint32_t is_period = 0;
 volatile uint8_t is_bit3Sample = 0;
@@ -8,12 +16,21 @@ volatile uint32_t is_lastTimeStamp = 0;
 volatile uint32_t is_minPeriod = 0xFFFFFFFF;
 volatile uint32_t is_maxPeriod = 0;
 volatile uint16_t is_currentBit = 0;
+// is_sampledBits is only used is_sampleInterrupt5 
+volatile uint16_t is_sampledBits = 0;
 volatile uint32_t is_noOfMisfire = 0;
 volatile uint32_t is_noOfRestarts = 0;
 
 volatile uint8_t is_bitBuffer[IS_BUFFER_SIZE];
 volatile uint32_t is_endOfBlockPeriod = 0xFFFFFFFF;  // This is the "end of block" marker
 volatile uint16_t is_bitsToLookFor = 0; 
+
+#if defined (__arm__) && defined (__SAM3X8E__) // Arduino Due compatible
+#define INTERRUPT_ID 2  // on due we must use the pin number
+#else
+#define INTERRUPT_ID 0  // on ordinary arduinos we have to use interrupt number
+#endif
+
 
 #ifdef __AVR__
 #define PIN3IN ((PIND>>3)&1)
@@ -73,22 +90,23 @@ void is_clearBuffer(void){
  * as "1" and "0" on the serial port
  */
 void is_printBuffer(uint16_t fromBit, uint16_t untilBit) {
-  if ((untilBit>>3) > IS_BUFFER_SIZE) {
-    Serial.print("Bit "); Serial.print(untilBit); Serial.println("won't fit the buffer");
-  }
+  
+  fromBit &= IS_BUFFER_MASK;
+  untilBit = (untilBit+1)& IS_BUFFER_MASK;
+
   int aByte = 0;
   int aBit = 0;
-  for (int i=fromBit; i<=untilBit; i++) {
+  for (int i=fromBit; i!=untilBit; ) {
     aByte = i >> 3;
     aBit = i & 0x7;
     Serial.print(is_bitBuffer[i]&bit(aBit)?"1":"0");
     
     if(i%64==0){
       Serial.println();
-    } else if(aBit==0){
+    } else if(aBit==7){
       Serial.print(",");
     } 
-    
+    i = (i+1) & IS_BUFFER_MASK; 
   }
   Serial.println();
 }
@@ -129,11 +147,7 @@ void is_storeBit(void) {
     // clear the sample bit (and the bits above as well)
     is_bitBuffer[aByte] &= 0xFF>>(8-aBit);
   }
-#if defined (__arm__) && defined (__SAM3X8E__) // Arduino Due compatible
-  is_currentBit = (is_currentBit+1)&0x1FFF; // 8192 bits
-#else
-  is_currentBit = (is_currentBit+1)&0xFFF; // 4096 bits
-#endif
+  is_currentBit = (is_currentBit+1)&IS_BUFFER_MASK;
 }
 
 void is_savePeriodStatistics(void) {
@@ -159,10 +173,10 @@ void is_sampleInterrupt1(void) {
 
 /**
  * Calculates the number of bits between each "end of block period"
+ * It does not actually sample anything.
  */
 void is_sampleInterrupt2(void) {
-  // get the time and data as quickly as possible
-  is_bit3Sample = PIN3IN;
+  // get the time as quickly as possible
   is_now = micros();
   
   if (DONE == is_samplerState) {
@@ -174,20 +188,18 @@ void is_sampleInterrupt2(void) {
   if (LOOKING_FOR_FIRST_GAP == is_samplerState) {
     if (is_period > is_endOfBlockPeriod){
       is_samplerState = LOOKING_FOR_SECOND_GAP;
-      is_storeBit();
+      is_currentBit = (is_currentBit+1)&IS_BUFFER_MASK;
     }
+    // ignore these bits
   } else if (LOOKING_FOR_SECOND_GAP == is_samplerState) {
     if (is_period > is_endOfBlockPeriod){
       is_samplerState = DONE;
       is_stopInterrupt();
-      
-      is_savePeriodStatistics();
       return;
     } else {
-      is_storeBit();
+      is_currentBit = (is_currentBit+1)&IS_BUFFER_MASK;
     }
-  } 
-  is_savePeriodStatistics();  
+  }
   is_lastTimeStamp = is_now;
 }
 
@@ -198,7 +210,7 @@ void is_sampleInterrupt2(void) {
  *   - or enconter an 'end of block' period. Then we just start over.
  */
 void is_sampleInterrupt3(void) {
-  // get the time and data as quickly as possible
+  // get the time and data sample as quickly as possible
   is_bit3Sample = PIN3IN;
   is_now = micros();
   
@@ -223,13 +235,10 @@ void is_sampleInterrupt3(void) {
 }
 
 /**
- * Another 'real' sampler, now we know the number of bits to look for.
- * And the idle period. 
  * This sampler will first wait for the 'idle period' then start sampling.
- *
  */
 void is_sampleInterrupt4(void) {
-  // get the time and data as quickly as possible
+  // get the time and data sample as quickly as possible
   is_bit3Sample = PIN3IN;
   is_now = micros();
   
@@ -257,6 +266,40 @@ void is_sampleInterrupt4(void) {
       }
     }
   } 
+  is_lastTimeStamp = is_now;
+}
+
+/**
+ * This sampler will start sampling directly.
+ * When it detectes the idle period it tests if enough bits have been aquired.
+ * It will continue/restart sampling if not enough bits have been stored.
+ */
+void is_sampleInterrupt5(void) {
+  // get the time and data sample as quickly as possible
+  is_bit3Sample = PIN3IN;
+  is_now = micros();
+  
+  if (DONE == is_samplerState) {
+    is_noOfMisfire += 1;
+    return; // Should not happend
+  }
+  is_period = is_now - is_lastTimeStamp;
+ 
+  if (is_period > is_endOfBlockPeriod){
+    if ( is_sampledBits >= is_bitsToLookFor) {
+      // we're done
+      is_stopInterrupt();
+    } else {
+      // start over
+      is_currentBit = 0;
+      is_noOfRestarts += 1;
+      is_storeBit();
+      is_sampledBits+=1;
+    }
+  } else {
+    is_storeBit();
+    is_sampledBits+=1; 
+  }
   is_lastTimeStamp = is_now;
 }
 
@@ -294,11 +337,7 @@ void is_startInterrupt1(bool onRising) {
   is_lastTimeStamp = micros();
   is_noOfMisfire = 0;
   is_noOfRestarts = 0;
-#if defined (__arm__) && defined (__SAM3X8E__) // Arduino Due compatible
-  attachInterrupt(2, is_sampleInterrupt1, onRising?RISING:FALLING); // 2 is pin number
-#else
-  attachInterrupt(0, is_sampleInterrupt1, onRising?RISING:FALLING);
-#endif
+  attachInterrupt(INTERRUPT_ID, is_sampleInterrupt1, onRising?RISING:FALLING);
 }
 
 
@@ -311,11 +350,8 @@ void is_startInterrupt2(uint32_t endOfBlockPeriod, bool onRising) {
   is_lastTimeStamp = micros();
   is_noOfMisfire = 0;
   is_noOfRestarts = 0;
-#if defined (__arm__) && defined (__SAM3X8E__) // Arduino Due compatible  
-  attachInterrupt(2, is_sampleInterrupt2, onRising?RISING:FALLING); // 2 is pin number
-#else
-  attachInterrupt(0, is_sampleInterrupt2, onRising?RISING:FALLING);
-#endif
+
+  attachInterrupt(INTERRUPT_ID, is_sampleInterrupt2, onRising?RISING:FALLING);
 }
 
 void is_startInterrupt3(uint32_t endOfBlockPeriod, uint16_t bitsToLookFor, bool onRising) {
@@ -328,11 +364,7 @@ void is_startInterrupt3(uint32_t endOfBlockPeriod, uint16_t bitsToLookFor, bool 
   is_lastTimeStamp = micros();
   is_noOfMisfire = 0;
   is_noOfRestarts = 0;
-#if defined (__arm__) && defined (__SAM3X8E__) // Arduino Due compatible
-  attachInterrupt(2, is_sampleInterrupt3, onRising?RISING:FALLING); // 2 is pin number
-#else
-  attachInterrupt(0, is_sampleInterrupt3, onRising?RISING:FALLING);
-#endif
+  attachInterrupt(INTERRUPT_ID, is_sampleInterrupt3, onRising?RISING:FALLING);
 }
 
 void is_startInterrupt4(uint32_t endOfBlockPeriod, uint16_t bitsToLookFor, bool onRising) {
@@ -345,18 +377,25 @@ void is_startInterrupt4(uint32_t endOfBlockPeriod, uint16_t bitsToLookFor, bool 
   is_lastTimeStamp = micros();
   is_noOfMisfire = 0;
   is_noOfRestarts = 0;
-#if defined (__arm__) && defined (__SAM3X8E__) // Arduino Due compatible
-  attachInterrupt(2, is_sampleInterrupt4, onRising?RISING:FALLING); // 2 is pin number
-#else
-  attachInterrupt(0, is_sampleInterrupt4, onRising?RISING:FALLING);
-#endif
+  attachInterrupt(INTERRUPT_ID, is_sampleInterrupt4, onRising?RISING:FALLING);
 }
 
+void is_startInterrupt5(uint32_t endOfBlockPeriod, uint16_t bitsToLookFor, bool onRising) {
+  is_bitsToLookFor = bitsToLookFor;
+  is_endOfBlockPeriod = endOfBlockPeriod;
+  is_maxPeriod = 0;
+  is_minPeriod = 0xFFFFFFFF;
+  is_currentBit = 0;
+  is_samplerState = LOOKING_FOR_FIRST_GAP;
+  is_lastTimeStamp = micros();
+  is_noOfMisfire = 0;
+  is_noOfRestarts = 0;
+  is_sampledBits = 0;
+  attachInterrupt(INTERRUPT_ID, is_sampleInterrupt5, onRising?RISING:FALLING); // 2 is pin number
+}
+
+
 void is_stopInterrupt(void) {
-#if defined (__arm__) && defined (__SAM3X8E__) // Arduino Due compatible
-  detachInterrupt(2); // 2 is pin number
-#else
-  detachInterrupt(0);
-#endif
+  detachInterrupt(INTERRUPT_ID); // 2 is pin number
   is_samplerState = DONE;
 }
--- InterruptSampler.h
+++ InterruptSampler.h
@@ -2,20 +2,12 @@
 #define INTERRUPT_SAMPLER_INCLUDE
 #include <stdint.h>
 
-
-#if defined (__arm__) && defined (__SAM3X8E__) // Arduino Due compatible
-#define IS_BUFFER_SIZE 1024 //8192 bits
-#else
-#define IS_BUFFER_SIZE 512  //4096 bits
-#endif
-
 extern volatile uint32_t is_now;
 extern volatile uint32_t is_period;
 extern volatile uint8_t is_bit3Sample;
 extern volatile uint32_t is_minPeriod;
 extern volatile uint32_t is_maxPeriod;
 extern volatile uint16_t is_currentBit;
-extern volatile uint8_t is_bitBuffer[IS_BUFFER_SIZE]; // 4095 bits sample buffer
 extern volatile uint32_t is_minIdlePeriod;
 extern volatile uint32_t is_noOfMisfire;
 extern volatile uint32_t is_noOfRestarts;
@@ -24,6 +16,8 @@ void is_startInterrupt1(bool onRising);
 void is_startInterrupt2(uint32_t endOfBlockPeriod, bool onRising);
 void is_startInterrupt3(uint32_t endOfBlockPeriod, uint16_t bitsToLookFor, bool onRising);
 void is_startInterrupt4(uint32_t endOfBlockPeriod, uint16_t bitsToLookFor, bool onRising);
+void is_startInterrupt5(uint32_t endOfBlockPeriod, uint16_t bitsToLookFor, bool onRising);
+
 void is_stopInterrupt(void);
 
 void is_printState(void);
